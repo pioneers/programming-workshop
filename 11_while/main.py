8ee919533a907eccb2928733fbd8da4f58917895
scottnuma <scott.numamoto@gmail.com>
2018-03-06 23:57:39 -0800

    Implement while loops

---
+++ 11_while/main.py
@@ -0,0 +1,19 @@
+def countdown(n):
+    while n > 0:
+        print(n)
+        n = n - 1
+    print('Blastoff!')
+
+def countup(final_number):
+    n = 0
+
+    # What condition should we use?
+    while False:
+
+        # How will we change our variables?
+        print(n)
+    print('Blastoff!')
+
+countup(5)
+
+
